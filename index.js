07e523a10f7e5e53b6bfcf8c9c2e1c3e6ac9b546
420-Paliwal <harshpaliwal0420@gmail.com>
2025-08-20 16:39:47 +0530

    fully functional crud using mongodb

--- index.js
+++ index.js
@@ -1,67 +1,114 @@
 const express = require('express');
 const app = express();
 const port = 3000
-const users = require('./MOCK_DATA.json')
 const fs = require('fs')
+const mongoose = require('mongoose');
+const { timeStamp } = require('console');
 // express.json()
+
 app.use(express.urlencoded({
     extended: true
 }))
+
+app.use(express.json())
+
+
+const userSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    job_title : {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+}, {
+     timestamps: true
+    })
+
+mongoose.connect('mongodb://127.0.0.1:27017/youtube-app-1')
+.then(() => console.log("MongoDb is Connected Successfully"))
+.catch((err) => console.log("Error in MongoDB Connection : ", err))
+
+const User = mongoose.model("user", userSchema)
+
 app.get('/', (req, res) =>{
     res.send('hello world');
 })
 
 /* this is for browser which returns html  */
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
+    const allDbUsers = await User.find({}) 
     const html = `<ul>
-      ${users.map(user =>`<li>${user.first_name} </li>`).join('')
+      ${allDbUsers.map(user =>`<li>${user.email} </li>`).join('')
       } 
-    </ul>`;
-    res.send(html);
+      </ul>`;
+      res.send(html); 
 })
 /* this is for mobile developer which returns json  */
-app.get('/api/users', (req, res) => {
-    res.json(users);
+app.get('/api/users', async (req, res) => {
+    const allDbUsers = await User.find({});
+    return res.send(allDbUsers)
 })
 
-app.get('/users/:id', (req, res) => {
-    const user = users.find(user => user.id === Number(req.params.id))
-    if (!user) {
+app.get('/users/:id', async (req, res) => {
+    const result = await User.findById(req.params.id)
+    if (!result) {
         return res.status(404).json({ error: 'User not found' });
     }
-    const html = `<h1>${user.first_name} ${user.last_name}</h1>
-    <p>Email: ${user.email}</p>
-    <p>Gender: ${user.gender}</p>
-    <p>Job Title : ${user.job_title}</p>
+    const html = `<h1>${result.first_name} ${result.last_name}</h1>
+    <p>Email: ${result.email}</p>
+    <p>Gender: ${result.gender}</p>
+    <p>Job Title : ${result.job_title}</p>
     `;                             
     return res.send(html);    
 })
 
 app.get('/api/users/:id', (req, res) => {
-    const user = users.find(user => user.id === Number(req.params.id))
-    if (!user) {
-        return res.status(404).json({ error: 'User not found' });
+    const result = User.findById(req.params.id)
+    if (!result) {
+        return res.status(404).json({
+            message : "User Not Found"
+        })
     }
-    res.json(user);                                 
+    console.log(result)
+    return res.status(200).json({
+        message : "User Found"
+    })  
 })
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const body = req.body
-    users.push({
-        id: users.length + 1,
+    if (!body){
+        !body || 
+        !first_name ||
+        !last_name ||
+        !email ||
+        !gender || 
+        !job_title
+        
+    }   
+    const result =  await User.create({
         first_name: body.first_name,
         last_name: body.last_name,
         email: body.email,
         gender: body.gender,
-        job_title: body.job_title, 
+        job_title: body.job_title,
     })
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err)=>{
-        if (err){
-            console.error('Error writing to file', err);
-            return res.status(500).json({ error: 'Failed to save user' });
-        }
-        return res.status(201).json({message : "User Created Successfully"})
-    })
+    return res.status(201).json({ message : "User Created Successfully ", user_Id : result})
 })
 app.post('/api/users', (req, res) => {
     const body = req.body
@@ -82,46 +129,25 @@ app.post('/api/users', (req, res) => {
     })
 })
 
-app.patch('/users/:id', (req, res) => {
-    const userId = Number(req.params.id)
+app.patch('/users/:id', async(req, res) => {
+    const userId = req.params.id
     const body = req.body
-    const userIndex = users.findIndex(user => user.id === userId)
-    if (userIndex === -1) {
-        return res.status(404).json({ error: 'User not found' });
-    }
-    users[userIndex] = {
-        id: userId,
-        first_name: body.first_name,
-        last_name: body.last_name,
-        email: body.email,
-        gender: body.gender,
-        job_title: body.job_title,
-    }
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err) => {
-        if (err){
-            console.error('Error writing to file', err);
-            return res.status(500).json({ error: 'Failed to update user' });
-        }
-        console.log('User updated successfully');
+    await User.findByIdAndUpdate(userId, req.body).then(()=>{
+        return res.status(201).send("User updated successfully");
+    }).catch((err) => {
+        console.error('Error updating user:', err);
+        return res.status(500).json({ error: 'Failed to update user' });
     })
-    return res.send("User updated successfully");
 })
 
-app.delete('/users/:id', (req, res) => {
-    const userId = Number(req.params.id)
-    console.log(userId)
-    const userIndex = users.findIndex(user => user.id === userId)
-    if (userIndex == -1) {
-        return res.status(404).json({ error: 'User not found' });
-    }
-     users.splice(userIndex, 1)
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err) => {
-        if (err) {
-            console.error('Error writing to file', err);
+app.delete('/users/:id', async (req, res) => {
+    const userId = req.params.id
+     User.findByIdAndDelete(userId).then(() => {
+         res.status(200).json({message : " User deleted successfully" });
+     }).catch((err) => {    
+            console.error('Error deleting user:', err);
             return res.status(500).json({ error: 'Failed to delete user' });
-        }
-    })
-    res.json({message : " User deleted successfully" });
+        })
 })
 
 app.delete('/api/users/:id', (req, res) => {
